94d0e695e0089902bc3a7f4667c9f26b2b85db18
NixxeN244 <ns233282@gmail.com>
2019-10-26 20:08:31 +0200

    genrating units
    changes made for the units to generate on the map

---
+++ RTS Game/Assets/scripts/MeleeUnit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeleeUnit : MonoBehaviour
+{
+    public GameObject Base1;
+    public GameObject Base2;
+    public GameObject Base3;
+
+    public GameObject enemy;
+    NavMeshAgent agent;
+    public float speed = 5f;
+    public float health = 20f;
+    public float attackDam = 5f;
+    
+    public GameObject thisObjectSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //agent = GetComponent<NavMeshAgent>();
+        //enemy = GameObject.FindGameObjectWithTag("ranger");
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject spawn = Instantiate(this.gameObject, Base1.transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
